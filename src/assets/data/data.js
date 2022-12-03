2ad2a21236eeb40f707e44a84a43ea20fc8dd783
Manoj <manojthulung03@gmail.com>
2022-12-03 20:34:57 +0545

    added level to 20

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -9,6 +9,15 @@ import Itachi from "../images/itachi.jpg";
 import Ichigo from "../images/ichigo.jpg";
 import Gintoki from "../images/gintoki.jpg";
 import Violet from "../images/violet.jpg";
+import Kakashi from "../images/kakashi.jpg";
+import Zoro from "../images/zoro.jpg";
+import Sakura from "../images/sakura.jpg";
+import Sanji from "../images/sanji.jpg";
+import Nami from "../images/nami.jpg";
+import Gohan from "../images/gohan.jpg";
+import Sasuke from "../images/sasuke.jpg";
+import Tanjiro from "../images/tanjiro.jpg";
+import Hinata from "../images/hinata.jpg";
 
 export const LEVELS = [
   {
@@ -76,5 +85,59 @@ export const LEVELS = [
     name: "violet",
     buttons: "PVIOOLNAERT",
     image: Violet,
+  },
+  {
+    level: 12,
+    name: "kakashi",
+    buttons: "KABAKAIPUSH",
+    image: Kakashi,
+  },
+  {
+    level: 13,
+    name: "zoro",
+    buttons: "NOWARZO",
+    image: Zoro,
+  },
+  {
+    level: 14,
+    name: "sakura",
+    buttons: "ASIRKUANR",
+    image: Sakura,
+  },
+  {
+    level: 15,
+    name: "sanji",
+    buttons: "SCAOONKIJ",
+    image: Sanji,
+  },
+  {
+    level: 16,
+    name: "nami",
+    buttons: "IMOKNARU",
+    image: Nami,
+  },
+  {
+    level: 17,
+    name: "gohan",
+    buttons: "GRAINNHO",
+    image: Gohan,
+  },
+  {
+    level: 18,
+    name: "hinata",
+    buttons: "LHIBNSATNA",
+    image: Hinata,
+  },
+  {
+    level: 19,
+    name: "tanjiro",
+    buttons: "JWIORAGNTOR",
+    image: Tanjiro,
+  },
+  {
+    level: 20,
+    name: "sasuke",
+    buttons: "SAUWGSAKE",
+    image: Sasuke,
   },
 ];
